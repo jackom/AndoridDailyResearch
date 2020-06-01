9a0bdde5f48171867501ddc5ba78aa596cef7880
zhengminxin <minxinyond@163.com>
2020-06-01 11:58:46 +0800

    提交测试代码

---
+++ app/src/main/java/com/example/testdemos/tmptest/Person1.kt
@@ -0,0 +1,17 @@
+package com.example.testdemos.tmptest
+
+/**
+ * @author : zhengminxin
+ * @date : 6/1/2020 11:22 AM
+ * @desc :
+ */
+class Person1 {
+
+    var name: String
+        get() {
+            return name
+        }
+        set(value) {
+            name = value
+        }
+}
---
+++ app/src/main/java/com/example/testdemos/tmptest/TmpTest1.kt
@@ -0,0 +1,17 @@
+package com.example.testdemos.tmptest
+
+/**
+ * @author : zhengminxin
+ * @date : 6/1/2020 11:21 AM
+ * @desc :
+ */
+fun main() {
+
+    test1()
+}
+
+private fun test1() {
+    val person1 = Person1()
+    person1.name = "jack"
+    println(person1.name)
+}
