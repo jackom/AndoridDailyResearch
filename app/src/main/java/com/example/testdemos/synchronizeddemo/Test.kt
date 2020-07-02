70a9102841b842c5624c8e3c0a57d99128f8995d
zhengminxin <minxinyond@163.com>
2020-07-02 22:29:57 +0800

    屏蔽掉maven仓库相关

--- app/src/main/java/com/example/testdemos/synchronizeddemo/Test.kt
+++ app/src/main/java/com/example/testdemos/synchronizeddemo/Test.kt
@@ -1,13 +1,9 @@
 package com.example.testdemos.synchronizeddemo
 
-import com.example.testmaven.LogUtils
-import com.example.testremotemaven.TestRemoteMaven
-import com.example.testupload2jcenter.TestUpload2Jcenter
-
 fun main() {
 //    LogUtils.d("Test.kt", "test maven constructor")
 //    TestRemoteMaven.print()
-    TestUpload2Jcenter().test()
+//    TestUpload2Jcenter().test()
     println()
     val count = Count()
     val t1 = Thread(count)
