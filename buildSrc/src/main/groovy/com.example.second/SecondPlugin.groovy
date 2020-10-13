ac2cafaae7a187fc1e78adacabf6f1502a944944
zhengminxin <minxinyond@163.com>
2020-10-13 23:15:54 +0800

    更新gradle版本

--- buildSrc/src/main/groovy/com.example.second/SecondPlugin.groovy
+++ buildSrc/src/main/groovy/com.example.second/SecondPlugin.groovy
@@ -3,12 +3,12 @@ package com.example.second
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-public class SecondPlugin implements Plugin<Project> {
+class SecondPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
 
-        def myExtension = project.extensions.create('pluginsrc', MyExtension)
+        def myExtension = project.extensions.create('secondPluginTest', MyExtension)
 
         println("========================")
         project.task('testPlugin123') {
