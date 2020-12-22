48c8575dcfac03dd9d151c0c9195c4922d20e517
zhengminxin <minxinyond@163.com>
2020-12-22 17:16:54 +0800

    提交代码

---
+++ app/src/main/java/com/example/testdemos/tmptest/A.java
@@ -0,0 +1,16 @@
+package com.example.testdemos.tmptest;
+
+/**
+ * @author : zhengminxin
+ * @date : 12/22/2020 5:03 PM
+ * @desc :
+ */
+public class A {
+    void show(A a) {
+        System.out.println("AA");
+    }
+
+    void show(D d) {
+        System.out.println("AD");
+    }
+}
---
+++ app/src/main/java/com/example/testdemos/tmptest/B.java
@@ -0,0 +1,15 @@
+package com.example.testdemos.tmptest;
+
+/**
+ * @author : zhengminxin
+ * @date : 12/22/2020 5:05 PM
+ * @desc :
+ */
+public class B extends A {
+    void show(A a) {
+        System.out.println("BA");
+    }
+    void show(B b) {
+        System.out.println("BB");
+    }
+}
